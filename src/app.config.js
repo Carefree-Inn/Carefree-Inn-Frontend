19c5c6f1f6d621137770fc4e65666d57ecbc68d0
Old-Second <oldsecond2@qq.com>
2023-06-29 11:41:25 +0800

    fix: fix router

--- src/app.config.js
+++ src/app.config.js
@@ -15,13 +15,13 @@ export default ({
     {
       root: "moduleA",
       pages: [
-       'pages/Homepage/index',
-       "pages/EditInfo/index",
-       'pages/Likepost/index',
-       'pages/MyReply/index'
+        'pages/Homepage/index',
+        "pages/EditInfo/index",
+        'pages/Likepost/index',
+        'pages/MyReply/index'
       ],
       independent: false
-    }, 
+    },
     {
       root: "moduleB",
       pages: [
@@ -29,38 +29,36 @@ export default ({
         'pages/PostArticle/index',
         'pages/TopicArticles/index',
         'pages/InitUserInfo/index',
-        'pages/TopicSearchResult/index',
-        'pages/Article/index',
-        'pages/PostArticle/index'
+        'pages/TopicSearchResult/index'
       ],
     }
-    
+
 
   ],
-    tabBar: {
-      /* tab页面必须放在主包里 */
-      list: [
-        {
-          pagePath: 'pages/Topic/index',
-          text:'话题',
-          iconPath: 'Images/topic.png',
-          selectedIconPath: 'Images/topic-selected.png'
-        },
-        {
-          pagePath: 'pages/Ground/index',
-          text:'广场',
-          iconPath: 'Images/ground.png',
-          selectedIconPath: 'Images/ground-selected.png',
-  
-        },
-        {
-          pagePath: 'pages/Mine/index',
-          text:'个人',
-          iconPath: 'Images/mine.png',
-          selectedIconPath: 'Images/mine-selected.png'
-        }],
-        color: '#000000',
-        backgroundColor: '#F5E3E3',
-        selectedColor: '#D39999'
-      }
+  tabBar: {
+    /* tab页面必须放在主包里 */
+    list: [
+      {
+        pagePath: 'pages/Topic/index',
+        text: '话题',
+        iconPath: 'Images/topic.png',
+        selectedIconPath: 'Images/topic-selected.png'
+      },
+      {
+        pagePath: 'pages/Ground/index',
+        text: '广场',
+        iconPath: 'Images/ground.png',
+        selectedIconPath: 'Images/ground-selected.png',
+
+      },
+      {
+        pagePath: 'pages/Mine/index',
+        text: '个人',
+        iconPath: 'Images/mine.png',
+        selectedIconPath: 'Images/mine-selected.png'
+      }],
+    color: '#000000',
+    backgroundColor: '#F5E3E3',
+    selectedColor: '#D39999'
+  }
 })
